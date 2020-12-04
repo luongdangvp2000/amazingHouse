5c1e48930f287d2fa81438fd7cd2e9e786e3c994
luongdangvp2000 <luongdangvp2000@gmail.com>
2020-12-05 02:06:37 +0700

    add house template content

--- src/components/HouseTemplate.js
+++ src/components/HouseTemplate.js
@@ -1,14 +1,37 @@
 import './HouseTemplate.css'
 import React, { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import Image from 'react-bootstrap/Image'
 
 
 function HouseTemplate() {
     return (
         <>
             <h1>House Template</h1>
-            <div>
-                <img src='/images/apartment.jpg' alt="housetemplate" />
-            </div>
+            <Container>
+                <Row className="gardenHouse">
+                    <Col><Image className="gardenHouse-img" src='/images/gardenHouse.jpg' alt="gardenHouse" /></Col>
+                    <Col>
+                        <p>Trapped in the chaos of daily life and chores, most urban dwellers long for a place to find peace of mind and return to nature. As it is hard to find such a place in megacities, gardening house offers nature lovers a chance to take up gardening in the comfort of your own home</p>
+                    </Col>
+
+                </Row>
+                <Row className="aparment">
+
+                    <Col>
+                        <p>Nowaday, apartment is one of the most choices to live. Living in an apartment is great, but it can sometimes be a bit of a struggle when it comes to finding enough space for all your stuff.</p>
+                    </Col>
+                    <Col><Image className="apartment-img" src='/images/apartment.jpg' alt="apartment" /></Col>
+                </Row>
+                <Row className="miniApartment">
+                    <Col><Image className="miniApartment-img" src="/images/miniApartment.jpg" alt="miniApartment" /></Col>
+                    <Col>
+                        <p>Mini Apartment is a new trending of young living style. A big part of young people such as student, single person, ... usually choose this living style cause it's cheap and convenient</p>
+                    </Col>
+                </Row>
+
+            </Container>
+
         </>
     );
 }
