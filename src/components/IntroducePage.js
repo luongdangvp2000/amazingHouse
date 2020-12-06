2a5add2ed48270f1e7f715fa27ee53e9069bf394
luongdangvp2000 <luongdangvp2000@gmail.com>
2020-12-07 00:46:20 +0700

    add form contact us

--- src/components/IntroducePage.js
+++ src/components/IntroducePage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Jumbotron from 'react-bootstrap/Jumbotron'
+import './IntroducePage.css'
 
 function IntroducePage() {
     return (
         <Jumbotron>
-            <div>
-                <h1>About Us Page</h1>
+            <div className="text-center">
+                <br />
+                <h1 >About Us Page</h1>
                 <p>We try to build a website for everyone to rent house. Help many people make us feel very happy and meaningful. Day by day, we are together to make our life more beautiful.</p>
             </div>
         </Jumbotron>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,27 +1,36 @@
 import Form from 'react-bootstrap/Form'
-import {Row, Col} from 'react-bootstrap'
+import { Row, Col, Jumbotron } from 'react-bootstrap'
 
 function Contact() {
     return (
-        <Form>
-            <Form.Group as={Row} controlId="formHorizontalEmail">
-                <Form.Label column sm={2}>
-                    Email
-                </Form.Label>
-                <Col sm={10}>
-                    <Form.Control type="email" placeholder="Email" />
-                </Col>
-            </Form.Group>
+        <div>
+            <Jumbotron>
+                <br />
+                <h1>Contact Us</h1>
+                <p>Swing by for a cup of coffee, or leave us a message:</p>
+            </Jumbotron>
 
-            <Form.Group as={Row} controlId="formHorizontalPassword">
-                <Form.Label column sm={2}>
-                    Password
+            <Form>
+                <Form.Group as={Row} controlId="formHorizontalEmail">
+                    <Form.Label column sm={2}>
+                        Email
                 </Form.Label>
-                <Col sm={10}>
-                    <Form.Control type="password" placeholder="Password" />
-                </Col>
-            </Form.Group>
-        </Form>
+                    <Col sm={10}>
+                        <Form.Control type="email" placeholder="Email" />
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} controlId="formHorizontalPassword">
+                    <Form.Label column sm={2}>
+                        Password
+                </Form.Label>
+                    <Col sm={10}>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Col>
+                </Form.Group>
+            </Form>
+        </div>
+
     );
 }
 
