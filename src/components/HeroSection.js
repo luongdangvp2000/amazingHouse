76c8bc84b0cfac3164976389d2b8ef1ad75ad01c
luongdangvp2000 <luongdangvp2000@gmail.com>
2020-12-09 14:09:14 +0700

    add dropdown to search bar

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,37 +2,55 @@ import React, { Component } from 'react'
 import './HeroSection.css'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownItem from 'react-bootstrap/esm/DropdownItem'
+import Button from 'react-bootstrap/Button'
 
 
 function HeroSection() {
     return (
         <div className="hero-container">
             <div className="hero-child">
-                <h1 className="introduce">Welcome</h1>
-                <p className="introduce">Let's rent a house for you</p>
-                <form className="form-inline my-2 my-lg-0 text-center" id="search-bar">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search you address" aria-label="Search" />
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                </form>
+                <div>
+                    <h1 className="introduce">Welcome</h1>
+                    <p className="introduce">Let's rent a house for you</p>
+                    <form className="form-inline my-2 my-lg-0 text-center" id="search-bar">
+                        <input className="form-control mr-sm-2" type="search" placeholder="Search your location" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
 
-                <Dropdown>
-                    <Dropdown.Toggle variant="success" id="dropdown-basic">
-                        Dropdown Button
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-                <Dropdown>
-                    <Dropdown.Toggle >
-                        
-                    </Dropdown.Toggle>
-                </Dropdown>
+                <div className="dropdown-group">
+                    <Dropdown className="col-4">
+                        <Dropdown.Toggle as={Button} className="btn btn-light btn-block">Province</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <button class="dropdown-item" type="button">Action</button>
+                            <button class="dropdown-item" type="button">Another action</button>
+                            <button class="dropdown-item" type="button">Something else here</button>
+                        </Dropdown.Menu>
+                    </Dropdown>
 
+                    <Dropdown className="col-4">
+                        <Dropdown.Toggle as={Button} className="btn btn-light btn-block">District</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <button class="dropdown-item" type="button">Action</button>
+                            <button class="dropdown-item" type="button">Another action</button>
+                            <button class="dropdown-item" type="button">Something else here</button>
+                        </Dropdown.Menu>
+                    </Dropdown>
+
+                    <Dropdown className="col-4">
+                        <Dropdown.Toggle as={Button} className="btn btn-light btn-block">Street</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <button class="dropdown-item" type="button">Action</button>
+                            <button class="dropdown-item" type="button">Another action</button>
+                            <button class="dropdown-item" type="button">Something else here</button>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
             </div>
-            
+
+
+
 
         </div>
     );
