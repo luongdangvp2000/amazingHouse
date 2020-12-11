74e3e478fd81e50d483ff94f99901895e0a8a5c6
luongdangvp2000 <luongdangvp2000@gmail.com>
2020-12-12 00:18:22 +0700

    add admin-react package

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,49 +4,66 @@ import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownItem from 'react-bootstrap/esm/DropdownItem'
 import Button from 'react-bootstrap/Button'
+import { Container, Row, Col } from 'react-bootstrap'
 
 
 function HeroSection() {
     return (
         <div className="hero-container">
             <div className="hero-child">
-                <div>
-                    <h1 className="introduce">Welcome</h1>
-                    <p className="introduce">Let's rent a house for you</p>
-                    <form className="form-inline my-2 my-lg-0 text-center" id="search-bar">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search your location" aria-label="Search" />
+
+                <h1 className="introduce">Welcome</h1>
+                <p className="introduce">Let's rent a house for you</p>
+
+                <Row>
+                    <Col sm={9}>
+                        <input className="form-control mr-sm-2" size="50" type="search" placeholder="Search your location" aria-label="Search" />
+                    </Col>
+                    <Col sm={3}>
                         <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
+                    </Col>
+                </Row>
 
-                <div className="dropdown-group">
-                    <Dropdown className="col-4">
-                        <Dropdown.Toggle as={Button} className="btn btn-light btn-block">Province</Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            <button class="dropdown-item" type="button">Action</button>
-                            <button class="dropdown-item" type="button">Another action</button>
-                            <button class="dropdown-item" type="button">Something else here</button>
-                        </Dropdown.Menu>
-                    </Dropdown>
+                <Row>
+                    <Col sm>
+                        <Dropdown>
+                            <Dropdown.Toggle as={Button} className="btn btn-light btn-block">Province</Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <button class="dropdown-item" type="button">Action</button>
+                                <button class="dropdown-item" type="button">Another action</button>
+                                <button class="dropdown-item" type="button">Something else here</button>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Col>
+                    <Col sm>
+                        <Dropdown>
+                            <Dropdown.Toggle as={Button} className="btn btn-light btn-block">District</Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <button class="dropdown-item" type="button">Action</button>
+                                <button class="dropdown-item" type="button">Another action</button>
+                                <button class="dropdown-item" type="button">Something else here</button>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Col>
+                    <Col sm>
+                        <Dropdown>
+                            <Dropdown.Toggle as={Button} className="btn btn-light btn-block">Street</Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <button class="dropdown-item" type="button">Action</button>
+                                <button class="dropdown-item" type="button">Another action</button>
+                                <button class="dropdown-item" type="button">Something else here</button>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Col>
+                </Row>
 
-                    <Dropdown className="col-4">
-                        <Dropdown.Toggle as={Button} className="btn btn-light btn-block">District</Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            <button class="dropdown-item" type="button">Action</button>
-                            <button class="dropdown-item" type="button">Another action</button>
-                            <button class="dropdown-item" type="button">Something else here</button>
-                        </Dropdown.Menu>
-                    </Dropdown>
 
-                    <Dropdown className="col-4">
-                        <Dropdown.Toggle as={Button} className="btn btn-light btn-block">Street</Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            <button class="dropdown-item" type="button">Action</button>
-                            <button class="dropdown-item" type="button">Another action</button>
-                            <button class="dropdown-item" type="button">Something else here</button>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </div>
+
+
+
+
+
+
             </div>
         </div>
     );
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -4,24 +4,53 @@ import Form from 'react-bootstrap/Form'
 
 function SignUp() {
     return (
-        <div>
-            <h3>Sign Up</h3>
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
-                </Form.Group>
+        <div className="bg-light">
+            <div>
+                <h3>Sign Up</h3>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter your name" />
+                    </Form.Group>
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">Register</Button>
-            </Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>CMT/CMND</Form.Label>
+                        <Form.Control type="text" placeholder="Enter your code" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Address</Form.Label>
+                        <Form.Control type="text" placeholder="Enter your address" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Number Phone</Form.Label>
+                        <Form.Control type="text" placeholder="Enter your number" />
+
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Enter your email" />
+
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" block>Register</Button>
+                </Form>
+            </div>
         </div>
     );
 }
